f900c496e80a70da1f47975b3b97ef237fb9e52f
Pricila-santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-08-04 02:03:29 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,10 +5,11 @@ import os
 
 #iniciar = input("Bem vindo! Gostaria de jogar uma partida de Backjack? Digite 'S' ou 'N'").lower()
 
-cartas = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
 
 def mao_carta1():
-    carta_recebida = random.choices(cartas, k= 2)
+    cartas = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    carta_recebida = random.choice(cartas)
     return carta_recebida
 
 def soma_mao(cartas):
@@ -24,23 +25,33 @@ def compare():
         if soma_jogador1 == 0:
             print(f"Você tem um Blackjack{jogador1}. Parabéns você ganhou!!")
         elif soma_jogador1 == 0 and soma_cpu == 0:
-            print(f"Seu adversário e você tem a mesma pontuação = {soma_jogador1}. Empatou!!")
+            print(f"Seu adversário e você tem um Blackjack = {jogador1}. Empatou!!")
         elif soma_cpu == 0:
-            print(f"Você recebeu as cartas: {jogador1}\nSua pontuação atual é {soma_jogador1}.")
-            print(f"Seu Adverário tem um Blackjack{mao_carta1}. Você perdeu!")
+            print(f"Seu Adverário tem um Blackjack{cpu}. Você perdeu!")
         elif soma_jogador1 == 21:
             print(f"Suas cartas são {jogador1} e a soma delas é {soma_jogador1}. Parabens você venceu!")
         elif soma_cpu == 21:
             print(f"Seu adverssário recebeu as cartas {cpu} e a soma delas é {soma_cpu}. Você perdeu!")
+        elif soma_cpu == soma_jogador1:
+            print(f"Voce recebeu as cartas {jogador1} e a soma delas é {soma_jogador1} o mesmo de seu adversário {cpu}. Empatou!")
+        
             
-        else:
-            print(f"Você recebeu as cartas: {jogador1}\nSua pontuação atual é {soma_jogador1}.")
-            print(f"A primeira das duas cartas do seu adversário é {cpu[0]}.")
 
-cpu = mao_carta1()
-jogador1 = mao_carta1()
+cpu = []
+jogador1 = []
+
+for _ in range(2): #passa o código duas vezes;
+    cpu.append(mao_carta1())
+    jogador1.append(mao_carta1())
+print(cpu)
+print(jogador1)
+
 soma_jogador1 = soma_mao(jogador1)
 soma_cpu = soma_mao(cpu)
+
+print(f"Você recebeu as cartas: {jogador1}\nSua pontuação atual é {soma_jogador1}.\n")
+print(f"A primeira das duas cartas do seu adversário é {cpu[0]}.\n")
+
 compare()
 
 
