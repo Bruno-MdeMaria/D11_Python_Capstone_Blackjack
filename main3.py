a4131beef733a6f6d985889c0639f15f6cbae978
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-08-05 01:00:39 -0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -29,22 +29,24 @@ def soma_mao(cartas):
 
 
 #Dica 13: Crie uma função chamada compare() e passe o user_score e computer_score. Se o computador e o usuário tiverem a mesma pontuação, é um empate. Se o computador tiver um blackjack (0), o usuário perde. Se o usuário tiver um blackjack (0), então o usuário ganha. Se o user_score for superior a 21, o usuário perde. Se o computer_score for superior a 21, o computador perde. Se nenhuma das opções acima, então o jogador com a maior pontuação ganha.
-def compare():
+def compare(soma_jogador1, soma_cpu):
+        if soma_jogador1 > 21 and soma_cpu > 21:
+            return f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} A soma do seu adverário é: {soma_cpu}. Você perdeu!"
         if soma_cpu == 0:
-            print(f"Seu Adverário tem um Blackjack{cpu}. Você perdeu!")
+            return f"Seu Adverário tem um Blackjack{cpu}. Você perdeu!"
         elif soma_jogador1 == 0:
-            print(f"Você tem um Blackjack{jogador1}. Parabéns você ganhou!!")
+            return f"Você tem um Blackjack{jogador1}. Parabéns você ganhou!!"
         elif soma_jogador1 == 0 and soma_cpu == 0:
-            print(f"Seu adversário e você tem um Blackjack = {jogador1}. Empatou!!")
+            return f"Seu adversário e você tem um Blackjack = {jogador1}. Empatou!!"
         elif soma_jogador1 > 21:
-            print(f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1}. Você perdeu!")
+            return f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1}. Você perdeu!"
         elif soma_cpu > 21:
-            print(f"Seu adverssário tem {cpu} e a soma delas é {soma_cpu}. Você ganhou!")
+            return f"Seu adverssário tem {cpu} e a soma delas é {soma_cpu}. Você ganhou!"
         elif soma_cpu == soma_jogador1:
-            print(f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} o mesmo de seu adversário {cpu}. Empatou!")
+            return f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} o mesmo de seu adversário {cpu}. Empatou!"
         elif soma_jogador1 > soma_cpu:
-            print(f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} A soma do seu adverário é: {soma_cpu}. Você ganhou!")
-        else: print(f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} A soma do seu adverário é: {soma_cpu}. Você perdeu!")
+            return f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} A soma do seu adverário é: {soma_cpu}. Você ganhou!"
+        else: return f"Suas cartas são: {jogador1} e a soma delas é {soma_jogador1} A soma do seu adverário é: {soma_cpu}. Você perdeu!"
         
 def continua_para():
     if soma_jogador1 < 21:
@@ -65,7 +67,8 @@ def cpu_joga():
 #Dica 5: Dê ao usuário e ao computador 2 cartas cada usando deal_card()
 cpu = []
 jogador1 = []
-continua = True
+fim_jogo = False
+
 
 for _ in range(2): #passa o código duas vezes;
     cpu.append(mao_carta1())
@@ -77,13 +80,16 @@ print(jogador1)
 
 #Dica 11: A pontuação deverá ser verificada novamente a cada nova carta retirada e as verificações da Dica 9 precisam ser repetidas até o fim do jogo.
 
-soma_jogador1 = soma_mao(jogador1)
-soma_cpu = soma_mao(cpu)
+while fim_jogo = False:
+    soma_jogador1 = soma_mao(jogador1)
+    soma_cpu = soma_mao(cpu)
 
-print(f"A primeira das duas cartas do seu adversário é {cpu[0]}.\n")
-print(f"Você recebeu as cartas: {jogador1}\nA soma de suas cartas é {soma_jogador1}.\n")
+    print(f"A primeira das duas cartas do seu adversário é {cpu[0]}.\n")
+    print(f"Você recebeu as cartas: {jogador1}\nA soma de suas cartas é {soma_jogador1}.\n")
 
-
+    if soma_jogador1 == 0 or soma_cpu == 0 or soma_jogador1 > 21:
+        fim_jogo = True
+  
 
 
 
